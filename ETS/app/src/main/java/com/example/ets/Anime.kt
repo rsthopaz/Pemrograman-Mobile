1f785dd386115bbdf690489f017de6f8bbc2dd82
rsthopaz <thopazrosadi15@gmail.com>
2026-04-18 19:29:20 +0700

    add class for anime

---
+++ ETS/app/src/main/java/com/example/ets/Anime.kt
@@ -0,0 +1,10 @@
+package com.example.ets
+
+data class Anime(
+    val title: String,
+    val imageUrl: String,
+    val releaseDate: String,
+    val genre: String,
+    val rating: String,
+    val synopsis: String
+)
--- ETS/app/src/main/java/com/example/ets/ui/theme/Anime.kt
+++
@@ -1,2 +0,0 @@
-package com.example.ets.ui.theme
-
