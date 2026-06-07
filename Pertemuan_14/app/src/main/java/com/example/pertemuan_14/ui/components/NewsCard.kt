eceafc962a1750854aebfd673da44ceb5e818f83
rsthopaz <thopazrosadi15@gmail.com>
2026-06-07 10:52:31 +0700

    update NewsCard

--- Pertemuan_14/app/src/main/java/com/example/pertemuan_14/ui/components/NewsCard.kt
+++ Pertemuan_14/app/src/main/java/com/example/pertemuan_14/ui/components/NewsCard.kt
@@ -1,2 +1,48 @@
 package com.example.pertemuan_14.ui.components
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.example.pertemuan_14.data.model.Article
+
+
+@Composable
+fun NewsCard(
+    article: Article,
+    onClick: () -> Unit
+) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .clickable {
+                onClick()
+            }
+    ) {
+        Column {
+            AsyncImage(
+                model = article.urlToImage,
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp)
+            )
+            Text(
+                text = article.title,
+                style =
+                    MaterialTheme.typography.titleMedium,
+                modifier =
+                    Modifier.padding(12.dp)
+            )
+        }
+    }
+}
