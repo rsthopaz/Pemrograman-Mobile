bc8a23f599426029247c39b55fabc3ea9c3ad3ad
rsthopaz <thopazrosadi15@gmail.com>
2026-06-07 09:07:05 +0700

    udpate class Article

--- Pertemuan_14/app/src/main/java/com/example/pertemuan_14/data/model/Article.kt
+++ Pertemuan_14/app/src/main/java/com/example/pertemuan_14/data/model/Article.kt
@@ -1,2 +1,10 @@
 package com.example.pertemuan_14.data.model
 
+data class Article(
+    val title: String,
+    val description: String?,
+    val content: String?,
+    val author: String?,
+    val urlToImage: String?,
+    val publishedAt: String
+)
