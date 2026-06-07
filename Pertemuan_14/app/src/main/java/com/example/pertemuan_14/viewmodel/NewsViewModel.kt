fbbccce04964cabcddf67c2b528672be6c701c11
rsthopaz <thopazrosadi15@gmail.com>
2026-06-07 10:50:22 +0700

    update class NewsViewModel

--- Pertemuan_14/app/src/main/java/com/example/pertemuan_14/viewmodel/NewsViewModel.kt
+++ Pertemuan_14/app/src/main/java/com/example/pertemuan_14/viewmodel/NewsViewModel.kt
@@ -1,2 +1,48 @@
 package com.example.pertemuan_14.viewmodel
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pertemuan_14.data.repository.NewsRepository
+import com.example.pertemuan_14.ui.state.NewsUiState
+
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+
+class NewsViewModel : ViewModel() {
+
+    private val repository = NewsRepository()
+
+    private val _uistate =
+        MutableStateFlow<NewsUiState>(
+            NewsUiState.Loading
+        )
+
+    val uistate = _uistate.asStateFlow()
+
+    init {
+        loadNews()
+    }
+
+    fun loadNews(){
+
+        viewModelScope.launch {
+
+            try {
+                _uistate.value = NewsUiState.Loading
+
+                val response = repository.getNews()
+
+                _uistate.value = NewsUiState.Success(
+                    response.articles
+                )
+            } catch (e: Exception){
+
+                _uistate.value = NewsUiState.Error(
+                    e.message ?: "Unknown Error"
+                )
+            }
+        }
+
+    }
+
+}
