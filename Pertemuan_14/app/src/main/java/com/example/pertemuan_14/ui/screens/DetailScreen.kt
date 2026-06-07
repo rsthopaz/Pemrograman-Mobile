91fc6e9e14d45bf889263df0f7ab94b83af9827f
rsthopaz <thopazrosadi15@gmail.com>
2026-06-07 11:03:27 +0700

    udpate DetailScreen

--- Pertemuan_14/app/src/main/java/com/example/pertemuan_14/ui/screens/DetailScreen.kt
+++ Pertemuan_14/app/src/main/java/com/example/pertemuan_14/ui/screens/DetailScreen.kt
@@ -1,2 +1,46 @@
 package com.example.pertemuan_14.ui.screens
 
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.example.pertemuan_14.data.model.Article
+
+@Composable
+fun DetailScreen(
+    article: Article
+) {
+    Column {
+        AsyncImage(
+            model = article.urlToImage,
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(250.dp)
+        )
+        Text(
+            article.title,
+            style =
+                MaterialTheme.typography.headlineSmall,
+            modifier =
+                Modifier.padding(16.dp)
+        )
+        Text(
+            article.description ?: "",
+            modifier =
+                Modifier.padding(16.dp)
+        )
+        Text(
+            article.content ?: "",
+            modifier =
+                Modifier.padding(16.dp)
+        )
+    }
+}
