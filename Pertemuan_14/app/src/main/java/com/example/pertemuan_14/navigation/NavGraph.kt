945b622a6740ed49e25f4430fd8a428bc773b8ff
rsthopaz <thopazrosadi15@gmail.com>
2026-06-07 11:04:52 +0700

    udpate route

--- Pertemuan_14/app/src/main/java/com/example/pertemuan_14/navigation/NavGraph.kt
+++ Pertemuan_14/app/src/main/java/com/example/pertemuan_14/navigation/NavGraph.kt
@@ -1,2 +1,29 @@
 package com.example.pertemuan_14.navigation
 
+
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.pertemuan_14.ui.screens.HomeScreen
+import com.example.pertemuan_14.viewmodel.NewsViewModel
+
+@Composable
+fun AppNavGraph() {
+    val navController =
+        rememberNavController()
+    val viewModel =
+        viewModel<NewsViewModel>()
+    NavHost(
+        navController = navController,
+        startDestination = "home"
+    ) {
+        composable("home") {
+            HomeScreen(
+                viewModel = viewModel
+            ) { article ->
+            }
+        }
+    }
+}
